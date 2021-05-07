9917f0c56203b9ab0715d1a9c6d79022da726ab6
Amro192 <amro2003.tariq@gmail.com>
2021-05-07 17:42:31 +0300

    fun basics 2

---
+++ _python/python_fundamentals/FunctionsBasic2/FunBasic2.py
@@ -0,0 +1,51 @@
+# 1-
+# def countD(num):
+#     number = []
+#     for x in range(num,-1,-1):
+#         number.append(x)
+#     return number
+# print(countD(4))        
+
+#-2
+# def recNum(ar):
+#     sec = ar[1]
+#     print(ar[0])
+#     return sec
+# print(recNum([1,5]))    
+
+#-3
+# def sumList(ar):
+#     var = 0
+#     for i in range(len(ar)):
+#         var = len(ar)
+#     sumi = ar[0] + var
+#     return sumi
+# print(sumList([-1,2,3,1,6]))
+
+#-4
+# def arrayS(ar):
+#     nar=[]
+#     for i in range(len(ar)):
+
+#         if ar[i]>ar[1]:
+#             nar.append(ar[i])
+
+#     for x in range(len(ar)):
+
+#         if len(nar)>2 or len(nar)==2:
+#             print(len(nar))
+#             return nar
+
+#         else:
+#             return False
+
+# print(arrayS([1,2,3,4,6,-1,35]))    
+
+#-5
+# def lenAval(num1,num2):
+#     number=[]
+#     for x in range(0,num1,1):
+#         number.append(num2)
+#     return number
+
+# print(lenAval(3,5))
