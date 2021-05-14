8904066dd63c3373e2ac97689183995fb7ff86da
Amro192 <amro2003.tariq@gmail.com>
2021-05-14 19:43:16 +0300

    HTML-Table

--- flask/flask fundementals/hello_flask/hello.py
+++ flask/flask fundementals/hello_flask/hello.py
@@ -1,6 +1,6 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+from flask import Flask ,render_template
+app = Flask(__name__)   
+@app.route('/')          
 def hello_world():
     return 'Hello World!'
     
@@ -10,6 +10,18 @@ def dojo():
 @app.route('/say/<name>')
 def say(name):
     return f"hi {name}" 
+
+@app.route('/lists')
+def render_lists():
+    
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("lists.html", random_numbers = [3,1,5], students = student_info)
+
 @app.route('/repeat/<name>/<number>')
 def repeat(name,number):
     post = " "
@@ -18,5 +30,5 @@ def repeat(name,number):
     return post 
 
 
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-            app.run(debug=True)    # Run the app in debug mode.
+if __name__=="__main__":    
+            app.run(debug=True)    
